d8f741c704b02bfcee09cc3f434fd0822dfb30a0
Alan R R Batista <alanrrb@gmail.com>
2015-02-15 23:21:10 -0200

    adding food_preference model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 FoodType.create!(name: "sobremesa")
 FoodType.create!(name: "jantar")
 FoodType.create!(name: "entrada")
+
+FoodPreference.create!(name:"vegetariana")
+FoodPreference.create!(name:"sem lactose")
+FoodPreference.create!(name:"sem gluten")
