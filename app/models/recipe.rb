2bf54b10777cffd7b76f1586dd7c53213435dfe0
Alan R R Batista <alanrrb@gmail.com>
2015-02-16 18:26:07 -0200

    adding relationship between recipe and user

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :food_preference
   belongs_to :food_type
   belongs_to :cuisine
+  belongs_to :user
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
