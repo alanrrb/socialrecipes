22d1b5728a5fd55c20a6582fd2cf112286fa904f
Alan R R Batista <alanrrb@gmail.com>
2015-02-15 23:10:55 -0200

    adding food_type model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215232731) do
+ActiveRecord::Schema.define(version: 20150216010826) do
+
+  create_table "food_types", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name",         limit: 255
