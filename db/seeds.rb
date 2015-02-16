ec41a961589aa982b9559add02af1cb0e87890b4
Alan R R Batista <alanrrb@gmail.com>
2015-02-15 23:55:42 -0200

    adding cuisine model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@ FoodType.create!(name: "sobremesa")
 FoodType.create!(name: "jantar")
 FoodType.create!(name: "entrada")
 
-FoodPreference.create!(name:"vegetariana")
-FoodPreference.create!(name:"sem lactose")
-FoodPreference.create!(name:"sem gluten")
+FoodPreference.create!(name: "vegetariana")
+FoodPreference.create!(name: "sem lactose")
+FoodPreference.create!(name: "sem gluten")
+
+Cuisine.create!(name: "italiana")
+Cuisine.create!(name: "mineira")
+Cuisine.create!(name: "japonesa")
