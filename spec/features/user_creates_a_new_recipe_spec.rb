8218a86c3a0b1e3fe0b3099bdcd40757b0dad68a
Alan R. R. Batista <alanrrb@gmail.com>
2015-03-09 14:25:40 -0300

    refactoring spec

--- spec/features/user_creates_a_new_recipe_spec.rb
+++ spec/features/user_creates_a_new_recipe_spec.rb
@@ -4,9 +4,9 @@ Warden.test_mode!
 
 feature "User create a new recipe" do
   scenario "successfully" do
-    FactoryGirl.create(:cuisine)
-    FactoryGirl.create(:food_preference)
-    FactoryGirl.create(:food_type)
+    create(:cuisine)
+    create(:food_preference)
+    create(:food_type)
 
     visit new_recipe_path
 
@@ -14,15 +14,15 @@ feature "User create a new recipe" do
 
     expect(page).to have_content 'New Recipe'
 
-    fill_in 'recipe[name]', with: "recipe name"
-    fill_in 'recipe[cooking_time]', with: "10"
-    fill_in 'recipe[serves]', with: "10"
-    select 'Fácil', from: 'Difficulty'
-    select 'vegan', from: 'recipe[food_preference_id]'
-    select 'breakfast', from: 'recipe[food_type_id]'
-    select 'japonese', from: 'recipe[cuisine_id]'
-    fill_in 'recipe[ingredients]', with: "fadfd adf adfd afdsf adfd "
-    fill_in 'recipe[procedure]', with: "fdfdfa fd fdaf dfa"
+    fill_in 'recipe[name]'         , with: "recipe name"
+    fill_in 'recipe[cooking_time]' , with: "10"
+    fill_in 'recipe[serves]'       , with: "10"
+    fill_in 'recipe[ingredients]'  , with: "fadfd adf adfd afdsf adfd "
+    fill_in 'recipe[procedure]'    , with: "fdfdfa fd fdaf dfa"
+    select 'Fácil'     , from: 'Difficulty'
+    select 'vegan'     , from: 'recipe[food_preference_id]'
+    select 'breakfast' , from: 'recipe[food_type_id]'
+    select 'japonese'  , from: 'recipe[cuisine_id]'
 
     click_button 'Create Recipe'
 
@@ -31,7 +31,7 @@ feature "User create a new recipe" do
 end
 
 def login_as_user
-  user = FactoryGirl.create(:user)
+  user = create(:user)
   fill_in 'user[email]', with: "test@test.com"
   fill_in 'user[password]', with: "12345678"
   click_button 'Log in'
